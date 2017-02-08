f8aeaaad662bf22e7432eaa6a9a4fa17080e07c2
maheshkandhari <mahesh.kandhari@gmail.com>
2017-02-08 16:53:01 +0530

    Modified code to run webservice on ec2

--- src/main/java/de/solutiontogo/actg/SpringRestApp.java
+++ src/main/java/de/solutiontogo/actg/SpringRestApp.java
@@ -3,15 +3,21 @@ package de.solutiontogo.actg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SpringBootApplication
 @EnableAutoConfiguration
-public class SpringRestApp {
+public class SpringRestApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringRestApp.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringRestApp.class);
     }
 }
